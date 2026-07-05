253862b5eb6dcd4e31b78f1e94240688c95c7284
elatedvortex <vortexelated@gmail.com>
2026-07-05 20:11:15 +0530

    cors change

--- aegis-backend/app/core/config.py
+++ aegis-backend/app/core/config.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
     CORS_ORIGINS: list[str] = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://insure-tech-gas1.vercel.app"
     ]
 
     ENVIRONMENT: str = "development"
